312fd0a7b54c6751d364bab7536f84917d2aa475
UrekMaz <dcmaureenmiranda@gmail.com>
2024-08-29 14:37:57 +0530

    merge

--- frontend/tsec/src/App.jsx
+++ frontend/tsec/src/App.jsx
@@ -5,12 +5,19 @@ import './App.css'
 import Login from './Login.jsx'
 import Events from './events/Events.jsx'
 import FundPieChart from './events/PieChart.jsx'
+// import Home from './Home/CrowdFundingChart.jsx'
+import Header from './assets/components/Header.jsx'
+import Footer from './assets/components/Footer.jsx'
 function App() {
   const crowdfund = 1000;  // Total crowdfund
   const fund = 500;     
   return (
     <>
       <FundPieChart crowdfund={crowdfund} fund={fund} />
+    <Login/>
+    {/* <Home/>  */}
+    {/* <Header/> */}
+    {/* <Footer/> */}
     </>
   )
 }
